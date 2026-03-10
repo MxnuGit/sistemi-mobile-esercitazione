8a9cf636468c3a029129518401e7b57d0657062c
Menghetz <manuelmenghetti0@gmail.com>
2026-03-10 15:57:45 +0100

    Add calendar fragment upload

--- app/src/main/java/com/example/myapplication/MyNewActivity.kt
+++ app/src/main/java/com/example/myapplication/MyNewActivity.kt
@@ -2,10 +2,12 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.fragments.CalendarFragment
 
 class MyNewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,11 @@ class MyNewActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_my_new)
 
-        Log.d("CALENDAR", "Calendario aperto")
+        findViewById<Button>(R.id.btn2)
+            .setOnClickListener {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, CalendarFragment())
+                    .commit()
+            }
     }
 }
