cfbadb4d4871ac8eb53a128cd4469b6b2d9b61dc
Menghetz <manuelmenghetti0@gmail.com>
2026-03-10 14:48:43 +0100

    Add button binding in main activity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnActivityMain)
+            .setOnClickListener {
+                Log.d("btnActivityMain", "Premuto il bottone in alto")
+            }
     }
 }
